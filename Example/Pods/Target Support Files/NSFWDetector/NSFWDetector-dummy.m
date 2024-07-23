02fb64ce4432cd00ac2fde306f8a066783c733e7
Fabian Ehlert <fabian.ehlert@lovoo.com>
2024-07-23 17:55:39 +0200

    Update Example project

--- Example/Pods/Target Support Files/NSFWDetector/NSFWDetector-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_NSFWDetector : NSObject
-@end
-@implementation PodsDummy_NSFWDetector
-@end
--- Example/Pods/Target Support Files/Pods-NSFWDetector_Tests/Pods-NSFWDetector_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NSFWDetector_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_NSFWDetector_Tests
-@end
